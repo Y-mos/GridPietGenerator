8cd54f9c60489a6891b826f6026b59deed1b5397
Y-mos <ymos.36e17a7047@gmail.com>
2022-12-31 14:18:05 +0900

    modules for args were completed

--- src/arg.hpp
+++ src/arg.hpp
@@ -29,58 +29,67 @@ public:
     }
     ~Arg(){}
 private:
+    const std::string OPERAND_FOUND="TRUE";
+    bool isError;
+    std::string errorMessage;
     std::map<std::string,std::string> key2value;
     std::map<std::string,std::string> key2desc;
+    std::map<std::string,bool> key2hasOperand;
     std::map<std::string,std::string> option2key;
     void copyTo(Arg& trg) const
     {
-        std::copy(key2value.begin(),key2value.end(),trg.key2value.begin());
-        std::copy(key2desc.begin(),key2desc.end(),trg.key2desc.begin());
-        std::copy(option2key.begin(),option2key.end(),trg.option2key.begin());
+        trg.isError=isError;
+        trg.errorMessage=errorMessage;
+        trg.key2value.clear();
+        trg.key2desc.clear();
+        trg.option2key.clear();
+        for(auto itr=key2value.begin();itr!=key2value.end();itr++)
+        {
+            trg.key2value[itr->first]=itr->second;
+        }
+        for(auto itr=key2desc.begin();itr!=key2desc.end();itr++)
+        {
+            trg.key2desc[itr->first]=itr->second;
+        }
+        for(auto itr=option2key.begin();itr!=option2key.end();itr++)
+        {
+            trg.option2key[itr->first]=itr->second;
+        }
+        for(auto itr=key2hasOperand.begin();itr!=key2hasOperand.end();itr++)
+        {
+            trg.key2hasOperand[itr->first]=itr->second;
+        }
     }
     void init()
     {
+        isError=false;
+        errorMessage="";
         key2value.clear();
         key2desc.clear();
         option2key.clear();
+        key2hasOperand.clear();
     }
-private:
-    int appendOption(const std::string option, const std::string key, const std::string desc="")
+public:
+    int registerKey(const std::string key, const std::string desc, bool hasOperand)
+    {
+        key2value[key]="";
+        key2desc[key]=desc;
+        key2hasOperand[key]=hasOperand;
+    }
+    int addOption(const std::string option, const std::string key)
     {
         int ret=0;
-        if(option2key.find(option)==option2key.end())
+        if(key2value.find(key)!=key2value.end())
         {
             option2key[option]=key;
-            ret=option2key.size();
-            if(key2value.find(key)==key2value.end())
-            {
-                key2value[key]="";
-            }
-            if(desc!="")
-            {
-                key2desc[key]=desc;
-            }
-        }
-        else
-        {
-            std::cerr << "!Error: Option " << option << " has already been associated with key " << key << "." << std::endl;
-            ret=0;
-        }
-        
-        return ret;
-    }
-    int removeOptions()
-    {
-        int ret=0;
-        while(option2key.size()>0)
-        {
-            option2key.erase(option2key.begin());
         }
         return ret;
     }
-    int clearValues()
+    int resetValues()
     {
         int ret=0;
+        isError=false;
+        errorMessage="";
         for(auto itr=key2value.begin();itr!=key2value.end();itr++)
         {
             itr->second="";
@@ -88,10 +97,81 @@ private:
         }
         return ret;
     }
-    int parse(int argc, const char** argv)
+    int parse(int argc, char** argv)
     {
+        resetValues();
+        for(int i=1;i<argc;i++)
+        {
+            std::string arg=argv[i];
+            if(option2key.find(arg)!=option2key.end())
+            {
+                std::string key=option2key[arg];
+                if(key2value[key]!="")
+                {
+                    isError=true;
+                    errorMessage="One option was assigned twice. This error was triggered by option " +arg+ " ("+std::to_string(i)+")";
+                    break;
+                }
+                if(key2hasOperand[key])
+                {
+                    if(i+1<argc)
+                    {
+                        i++;
+                        key2value[key]=argv[i];
+                    }
+                    else
+                    {
+                        isError=true;
+                        errorMessage="No operand found for " + arg + "("+std::to_string(i)+")";
+                        break;
+                    }
+                }
+                else
+                {
+                    key2value[key]=OPERAND_FOUND;
+                }
+            }
+            else
+            {
+                isError=true;
+                errorMessage="Unknown option " + arg + "("+std::to_string(i)+")";
+                break;
+            }
+        }
         return 0;
     }
+    std::string get(std::string key) const
+    {
+        if(key2value.find(key)==key2value.end())
+        {
+            std::cerr << "!ERROR: No key named " << key << " found." << std::endl;
+            return "";
+        }
+        return key2value.at(key);
+    }
+    std::string resultList() const
+    {
+        std::stringstream ss;
+        if(isError)
+        {
+            ss << "ERROR:" << errorMessage << std::endl;
+        }
+        else
+        {
+            ss << "List of args:" << std::endl;
+            for(auto itr=key2value.begin();itr!=key2value.end();itr++)
+            {
+                if(itr->second=="")
+                {
+                    continue;
+                }
+                ss << "  - " << (itr->first) << " = " << (itr->second) << std::endl;
+            }
+        }
+        
+        return ss.str();
+    }
+    
     std::string help() const
     {
         std::stringstream ss;
@@ -100,7 +180,7 @@ private:
         
         for(auto itrK=key2value.begin();itrK!=key2value.end();itrK++)
         {
-            std::string key=itrK->first;
+            const std::string& key=itrK->first;
             std::map<std::string,int> options;
             for(auto itr=option2key.begin();itr!=option2key.end();itr++)
             {
@@ -114,7 +194,7 @@ private:
             {
                 ss << " " << (itr->first);
             }
-            ss << std::endl;
+            ss << (key2hasOperand.at(key)?" [arg]":" ") << std::endl;
             ss << "   " << key2desc.at(key) << std::endl;
         }
         
--- src/testArg.cpp
+++ src/testArg.cpp
@@ -5,12 +5,17 @@
 int main(int argc, char** argv)
 {
     Arg arg;
-    arg.appendOption("-i", "inputFileName");
-    arg.appendOption("--inputFileName", "inputFileName");
-    arg.appendOption("-o", "outputFileName");
-    arg.appendOption("--outputFileName", "outputFileName");
-    
+    arg.registerKey("inputFileName", "input file path", true);
+    arg.registerKey("outputFileName", "output file path", true);
+
+    arg.addOption("-i", "inputFileName");
+    arg.addOption("-o", "outputFileName");
+    arg.addOption("--inputFile", "inputFileName");
+    arg.addOption("--outputFile", "outputFileName");
+
     std::cerr << arg.help() << std::endl;
-    
+    arg.parse(argc, argv);
+    std::cerr << arg.resultList() << std::endl;
+
     return 0;
 }
